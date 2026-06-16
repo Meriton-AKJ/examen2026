91d4febfbe4a8401c4ed1310335c4d542574544d
Meriton Askaj <meritonaskaj01@gmail.com>
2026-06-16 19:21:35 +0200

    Debuggage de l'erreur côté Frontend

--- client/src/services/tasks.service.js
+++ client/src/services/tasks.service.js
@@ -1,4 +1,4 @@
-const API_URL = `${import.meta.env.VITE_BASE_URL}/tasks`;
+const API_URL = `${import.meta.env.VITE_API_URL}/tasks`;
 
 export const getTasks = async () => {
   const response = await fetch(API_URL);
