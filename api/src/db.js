c3825d7cb358136baa0566d8fe603939b11272aa
Meriton Askaj <meritonaskaj01@gmail.com>
2026-06-16 19:32:03 +0200

    Debuggage du côté back

--- api/src/db.js
+++ api/src/db.js
@@ -6,7 +6,7 @@ import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 const adapter = new PrismaMariaDb({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
+  user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   debug: false,
